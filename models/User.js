02ca0047b012e285b9866d6665e230e8c53d1f14
Tarun Singh <singhnikitarun@gmail.com>
2025-06-04 02:32:37 +0530

    adding env file

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/CRUD');
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = new mongoose.Schema({
     name: String,
